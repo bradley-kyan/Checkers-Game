65f6437897697661028b9f64cf1bdc4379372f60
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2023-04-26 16:17:23 +1200

    updateFile() and getPlayers done
    updateFile and getPlayers function should be done

--- CheckersGame/src/checkersgame/Player.java
+++ CheckersGame/src/checkersgame/Player.java
@@ -1,6 +1,12 @@
 package checkersgame;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -44,10 +50,29 @@ public class Player {
         this.losses = p.losses;
     }
 
-    public static void getPlayers()
-    {
-        //TODO populate playerList with an array of Players from the data stored
-        //in the file   -->  Use file i/o
+    public static void getPlayers() {
+        if (playerList != null) {
+            return; // don't load players if already loaded
+        }
+        playerList = new ArrayList<>();
+        File file = new File("players.txt");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] fields = line.split("\t"); // assuming tab-delimited fields
+                String name = fields[0];
+                Colour colour = Colour.valueOf(fields[1]);
+                int score = Integer.parseInt(fields[2]);
+                int wins = Integer.parseInt(fields[3]);
+                int losses = Integer.parseInt(fields[4]);
+                Player player = new Player(name, colour);
+                player.score = score;
+                player.wins = wins;
+                player.losses = losses;
+                playerList.add(player);
+            }
+        } catch (FileNotFoundException ex) {
+        }
     }
     public Player getPlayer(String name)
     {
@@ -58,8 +83,16 @@ public class Player {
     }
     public static void updateFile()
     {
-        //TODO method which will update the file with all the playerList contents
-        //updateFile and getPlayers must read/write to the file in the same format
+           
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter("players.txt"))) {
+        for (Player player : playerList) {
+            //Player's name, colour, score, wins, and losses to the file
+            writer.write(player.name + "," + player.colour.toString() + "," + player.score + "," + player.wins + "," + player.losses + "\n");
+        }
+    } catch (IOException e) {
+        System.out.println("An error occurred while writing the players to the file: " + e.getMessage());
+    }
+
     }
     
     public Colour getColour() {
